4fb4679ac949a7ecd7aa4e7aa9eb10b55d482540
drskiboss <skiboss@big-skis.com>
2025-02-06 14:32:22 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+export default defineConfig({
+  vite: {
+    define: {
+      "import.meta.env.DATOCMS_API_KEY": JSON.stringify(process.env.DATOCMS_API_KEY),
+    },
+  },
+});
+
+
 // https://astro.build/config
 export default defineConfig({});
